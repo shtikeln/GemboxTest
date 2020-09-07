d89074e818e51584ead019173f2b299f59823209
shtikel.n <shtikel.n@gmail.com>
2020-09-07 22:59:40 +0600

    Add project files.

---
+++ GemboxTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using GemBox.Document;
+using System;
+using System.IO;
+
+namespace GemboxTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new FileStream("input.docx", FileMode.Open))
+            {
+                ComponentInfo.SetLicense(
+                    "");
+                DocumentModel document = DocumentModel.Load(stream, new DocxLoadOptions());
+                using (var outPdf = new MemoryStream())
+                {
+                    document.Save("output.pdf");
+                }
+            }
+        }
+    }
+}
